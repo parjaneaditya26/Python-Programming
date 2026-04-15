f80ff4335d1473d658c4ec434a18295d872594fe
parjaneaditya26 <parjaneaditya26@gmail.com>
2026-04-15 18:52:13 +0530

    make a new files

---
+++ Basic Programs/comments.py
@@ -0,0 +1,5 @@
+# use for single line comment
+#print("Hello, World!") this is also a comment
+
+""" multi line comment
+this is a comment """
---
+++ Basic Programs/variables.py
@@ -0,0 +1,21 @@
+#variable is name of memory location which is used to store data. we can use variable to store any type of data like string, integer, float, boolean etc.
+# we can assign value to variable using assignment operator (=)
+# ruls for variable name:
+# 1. variable name must start with a letter or underscore
+# 2. variable name can contain letters, numbers and underscores
+# 3. variable name cannot be a keyword
+# 4. variable name cannot contain spaces
+# 5. variable name is not contain special characters except underscore
+# example of variable
+name = "aditya"
+num1=12
+num2=3.14
+is_student = True
+print(name)
+print(num1)
+print(num2)
+print(is_student)
+
+NameStudent="aditya" #pascal case
+Name_student="aditya" #camel case
+name_student="aditya" #snake case use more in python recommended
