7488085edea881599a55172f83e328469d1980bc
parjaneaditya26 <parjaneaditya26@gmail.com>
2026-05-05 20:30:39 +0530

    loops added

---
+++ loops.py
@@ -0,0 +1,34 @@
+#Loops in the python allow us to execute a block of code multiple times.
+
+# ---------------------for loop------------------------------------------ 
+# in that use range() function , range(start,stop,step)
+# range is always start with 0 upto n-1 and step is 1 . bur its changable
+sen="My name is Aditya"
+
+"""for i in range(1, 21, 2):
+    print(i)
+
+sen="My name is Aditya"
+for char in range(len(sen)):
+    print(sen[char])
+
+for i in sen:
+    print(i)"""
+
+#for j in range(len(sen)-1,-1,-1):
+#    print(sen[j])
+
+#-----------------Break ,continue and else statement------------------
+"""for i in range(0,20):
+    if i==15:
+        print("Break statement is executed ")
+        break
+    if i==10:
+        print("Continue statement is executed")
+        continue 
+
+    print(i)
+else:                                   
+    print("Else statement is executed ") #if for loop will not execute then else block will be executed """
+
+
---
+++ practice question/n_times.py
@@ -0,0 +1,3 @@
+num=int(input("How many times do you want to print the message?"))
+for i in range(1,num+1):
+    print("hello , ", i)
