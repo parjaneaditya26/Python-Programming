036d7a1a751d824b768b89297581964b044017a9
parjaneaditya26 <parjaneaditya26@gmail.com>
2026-05-26 22:35:25 +0530

    list

---
+++ list.py
@@ -0,0 +1,14 @@
+'''list is stores multiple data types item in a variable 
+list is ordered, changeable and allows duplicate values
+list is defined by using square brackets[]'''
+
+'''examle:- list_name=[1,2.3,"string",True]
+some of list methods are :- 
+1.append() - adds an item to the end of the list
+2.insert(index,item) - adds an item at the specified index
+3.remove(item) - removes a item from the list at first occurrance
+4.pop(index) - removes an item from the list at the specific index'''
+
+a=[1,2.3,4,4,5]
+print(a.count(5))#output: 2
+print(a)
